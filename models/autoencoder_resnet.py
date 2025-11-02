96b587a9da627bdcd7da94537988a7467e314647
voluch <voluch364@gmail.com>
2025-11-02 16:24:28 +0200

    Fix autoencoder architecture

--- models/autoencoder_resnet.py
+++ models/autoencoder_resnet.py
@@ -9,7 +9,6 @@ class ResidualBlock(nn.Module):
         self.relu = nn.ReLU(inplace=True)
         self.conv2 = nn.Conv2d(out_channels, out_channels, kernel_size=3, padding=1)
         self.bn2 = nn.BatchNorm2d(out_channels)
-        self.downsample = downsample
         if downsample or in_channels != out_channels:
             self.shortcut = nn.Conv2d(in_channels, out_channels, kernel_size=1, stride=stride)
         else:
@@ -30,31 +29,30 @@ class ResNetAutoencoder(nn.Module):
     def __init__(self, input_shape):
         super().__init__()
         self.input_shape = input_shape
-        # Encoder
-        self.enc1 = ResidualBlock(1, 32, downsample=False)
-        self.enc2 = ResidualBlock(32, 64, downsample=True)
-        self.enc3 = ResidualBlock(64, 128, downsample=True)
+        # Encoder (smaller)
+        self.enc1 = ResidualBlock(1, 16, downsample=False)
+        self.enc2 = ResidualBlock(16, 32, downsample=True)
+        self.enc3 = ResidualBlock(32, 64, downsample=True)
 
-        # Decoder (transpose conv upsample)
-        self.up1 = nn.ConvTranspose2d(128, 64, kernel_size=2, stride=2)
-        self.dec1 = ResidualBlock(64, 64)
-        self.up2 = nn.ConvTranspose2d(64, 32, kernel_size=2, stride=2)
-        self.dec2 = ResidualBlock(32, 32)
-        self.final = nn.Conv2d(32, 1, kernel_size=3, padding=1)
+        # Decoder (upsample to original shape)
+        self.up1 = nn.ConvTranspose2d(64, 32, kernel_size=2, stride=2)
+        self.dec1 = ResidualBlock(32, 32)
+        self.up2 = nn.ConvTranspose2d(32, 16, kernel_size=2, stride=2)
+        self.dec2 = ResidualBlock(16, 16)
+        self.final = nn.Conv2d(16, 1, kernel_size=3, padding=1)
         self.sigmoid = nn.Sigmoid()
 
     def forward(self, x):
-        e1 = self.enc1(x)
-        e2 = self.enc2(e1)
-        e3 = self.enc3(e2)
+        e1 = self.enc1(x)   # [B, 16, H, W]
+        e2 = self.enc2(e1)  # [B, 32, H/2, W/2]
+        e3 = self.enc3(e2)  # [B, 64, H/4, W/4]
 
-        d1 = self.up1(e3)
+        d1 = self.up1(e3)   # [B, 32, H/2, W/2]
         d1 = self.dec1(d1)
-        d2 = self.up2(d1)
+        d2 = self.up2(d1)   # [B, 16, H, W]
         d2 = self.dec2(d2)
-        out = self.final(d2)
+        out = self.final(d2)  # [B, 1, H, W]
         out = self.sigmoid(out)
-        # If needed, resize to input_shape (handles rounding)
         if out.shape[-2:] != self.input_shape:
             out = nn.functional.interpolate(out, size=self.input_shape, mode='bilinear', align_corners=False)
         return out
